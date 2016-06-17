daa99a04da241ecc3645fc4415bde6eceed15d37
Chris Alexander <chrisalex1101@gmail.com>
2016-06-17 15:46:12 +0000

    add css processing

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,44 @@ var config = getConfig({
   isDev: isDev,
   in: join(src, 'app.js'),
   out: dest,
-  clearBeforeBuild: true
+  clearBeforeBuild: true,
+  port: 8080,
+  hostname: "0.0.0.0"
 });
+
+
+/**
+ * CSS modules
+ */
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l && l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+};
+
+const cssloader = findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+});
+config.module.loaders.push(newloader);
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`);
+cssloader.loader = newloader.loader;
+// for CSS from npm dependencies i.e. font-awesome
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+});
+// postcss
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+]);
 
 module.exports = config;
